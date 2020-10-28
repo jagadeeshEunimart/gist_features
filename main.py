b7e1a4caab899c03c2fb01c5f55b3e8bbc42a034
jagadeeshEunimart <jagadeesh.vanga@eunimart.com>
2020-10-28 18:56:38 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from GIST import GIST
+from tqdm import tqdm
+import sys
+import argparse
+from PIL import Image
+import numpy as np
+import pandas as pd
+import re
+import feather
+import os
+import multiprocessing as mp
+
+
+param = {
+        "orientationsPerScale":np.array([8,8]),
+         "numberBlocks":[10,10],
+        "fc_prefilt":10,
+        "boundaryExtension":32
+}
+class Dataloader():
+	# def __init__(self,input_path):
+	# 	self.input_path = input_path
+	# 	# self.output_path = output_path
+	# 	self.is_dir = 1 if os.path.isdir(input_path) else 0
+	# 	print(self.is_dir)
+
+	def get_inputfile(self,input_path):
+		if os.path.isdir(input_path):
+			# dirctory in images
+			path = "{}".format(input_path)
+			a = sorted(os.listdir(path))
+			file_list = list(map(lambda x: path + x, a))
+
+			return file_list
+		else:
+			# image file such png, jpg etc..
+			path = "{}".format(input_path)
+			return [path]
+	# def save_feature(self,x):
+	# 	if self.is_dir:
+	# 		gist_df = pd.DataFrame(x, columns = [f"gist_{i}" for i in range(x.shape[1])])
+	# 	else:
+	# 		gist_df = pd.DataFrame(x.reshape(1,-1), columns = ["gist_{}".format(i) for i in range(x.shape[1])])
+
+	# 	gist_df.to_feather("./{}".format(self.output_path))	
+
+def _get_gist(param,file_list):
+	img_list = list(map(lambda f :np.array(Image.open(f).convert("L")), file_list))
+	gist = GIST(param)
+
+	with mp.Pool(mp.cpu_count()) as pool:
+		p = pool.imap(gist._gist_extract,img_list[:])
+		gist_feature = list(tqdm(p, total = len(img_list)))
+	return np.array(gist_feature)
+
+
+if __name__ == "__main__":
+
+	input_path = sys.argv[1]
+
+	data = Dataloader()
+	file_list = data.get_inputfile(input_path)
+	gist_feature = _get_gist(param,file_list)
+	print(gist_feature.shape)
+	# gist_feature = gist_feature.reshape(-1,1)
+	# print(gist_feature.shape)
+
+	# if args.save == True:
+		# data.save_feature(gist_feature)
